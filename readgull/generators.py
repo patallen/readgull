e054f2d99f0aa72b5eb431151807ae9c189c452f
Patrick Allen <prallen90@gmail.com>
2015-11-17 18:20:54 -0500

    Add get_content to return dict of content for a contenttype

--- readgull/generators.py
+++ readgull/generators.py
@@ -1,4 +1,5 @@
 import os
+from readers import BaseReader
 
 
 class Generator(object):
@@ -30,6 +31,7 @@ class Generator(object):
         self.content_type = self._get_content_name(content_type)
         self.content_path = self._get_content_path(self.content_type)
         self.filepaths = self._get_filepaths()
+        self.reader = BaseReader(settings)
 
     def _get_filepaths(self):
         """
@@ -39,7 +41,7 @@ class Generator(object):
         files = []
         for f in all_files:
             if f.endswith('.md'):
-                files.append(os.path.abspath(f))
+                files.append(os.path.join(self.content_path, f))
 
         return files
 
@@ -52,3 +54,16 @@ class Generator(object):
         return os.path.join(
             os.path.abspath(self.settings['PATH']),
             '{}s'.format(content_type))
+
+    def get_content(self):
+        content = {}
+
+        content[self.content_type + 's'] = {}
+
+        for file in self.filepaths:
+            # TODO: Somehow name the content appropriately
+            content[self.content_type + 's'][file] = self.reader.read(file)
+
+        return content
+
+
