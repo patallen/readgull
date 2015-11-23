ef3bf5b9b10742cadc7fd7607c43983fa929fad7
Patrick Allen <prallen90@gmail.com>
2015-11-23 18:56:08 -0500

    Have Content pull in settings and create local_settings

--- readgull/content.py
+++ readgull/content.py
@@ -1,3 +1,8 @@
+import copy
+
+from readgull.settings import DEFAULT_CONFIG
+
+
 class Content(object):
     """
     This object will contain the information, properties, and functions for
@@ -5,16 +10,26 @@ class Content(object):
     of this class will be accessible through dot notation.
     """
 
-    def __init__(self, content, metadata):
+    def __init__(self, content_name, content, metadata, settings=None):
+        self.content_name = content_name
         self.content = content
         self.local_metadata = metadata
         self._set_meta_attrs()
+
+        if settings is None:
+            settings = copy.deepcopy(DEFAULT_CONFIG)
+
+        self.settings = settings
+        self.local_settings = {}
+        if self.settings.get(self.content_name) is not None:
+            self.local_settings = self.settings[self.content_name]
 
     def _set_meta_attrs(self):
         """
         This method takes care of converting the metadata provided to
         attributes of the instance that can be accessed using dot notation.
         """
+
         if self.local_metadata:
             for key, value in self.local_metadata.iteritems():
                 setattr(self, key.lower(), value)
